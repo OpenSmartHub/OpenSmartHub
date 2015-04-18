605bf9786788769ce89df18b61bd98802f3e2d63
Anthony Ngu <anthony.ngu@outlook.com>
2015-04-18 16:39:42 -0700

    Re-organization of the code and removal of the combined-hub in favor of a split-hub.

--- combined-hub/automation/public_config.js
+++
@@ -1,5 +0,0 @@
-exports.WEATHER_UNDERGROUND_KEY = "your_key_here";
-exports.SPARK_USERNAME = 'USERNAME';
-exports.SPARK_PASSWORD = 'PASSWORD';
-exports.WEMO_SWITCH_NAME1 = 'WEMO_NAME1';
-exports.WEMO_SWITCH_NAME2 = "WEMO_NAME2";
--- split-hub/local/automation_modules/devices/spark/sparkInit.js
+++
@@ -1,7 +0,0 @@
-var spark = require('spark');
-
-exports.init = function(username, password)
-{
-  spark.login({username: username, password: password});
-  return spark;
-}
